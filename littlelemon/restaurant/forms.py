4fc4b37ab21cd3c5a13a170be50dbeb13ff319f7
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2026-01-07 03:16:48 +0100

    move handling past datetimes to clean_reserved_at method

--- littlelemon/restaurant/forms.py
+++ littlelemon/restaurant/forms.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2026-01-04 01:21:24
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2026-01-04 17:42:58
+# @Last Modified time: 2026-01-07 02:56:48
 """
 Forms module
 """
@@ -18,23 +18,20 @@ class ReservationForm(forms.ModelForm):
     """
     ReservationForm class
     """
+
     class Meta:  # pylint: disable=R0903, C0115
         model = Reservation
-        fields = ("first_name", "last_name", "no_guests", "reserved_at", "comment")
+        fields = ["first_name", "last_name", "no_guests", "reserved_at", "comment"]
 
-    def is_valid(self) -> bool:
-        if not super().is_valid():
-            return False
-
-        # handles a past datetime
+    def clean_reserved_at(self):
+        """
+        Handles past datetimes
+        """
         reserved_at = self.cleaned_data.get("reserved_at")
         if reserved_at and reserved_at < timezone.now():
-            self.add_error(
-                None, "Sorry, you cannot make a reservation for a past date and time."
+            raise forms.ValidationError(
+                "Cannot make a reservation for a past date and time."
             )
-            return False
-
-        return True
 
     def clean(self) -> dict[str, Any]:
         """
